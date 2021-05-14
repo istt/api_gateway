0dcd73c234aa132754f582f142f251f605a0d688
Dylan2011-pc <duyl0043@gmail.com>
2021-05-14 19:47:04 +0700

    deployments/docker/mongodb.yaml

--- internal/app/mongodb.go
+++ internal/app/mongodb.go
@@ -1,14 +1,60 @@
 package app
 
-// Create global mongoDB connection, so that other components can access with app.MongoDB.
-// var MongoDB
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/knadh/koanf/providers/confmap"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	// Create global mongoDB connection, so that other components can access with app.MongoDB.
+	// var MongoDB
+	MongoClient *mongo.Client
+	MongoDB     *mongo.Database
+)
 
 // MongoDBConfig provide default settings for MongoDB
 func MongoDBConfig() {
+	Config.Load(confmap.Provider(map[string]interface{}{
+		"mongodb.user":     "root",
+		"mongodb.password": "",
+		"mongodb.host":     "localhost27017",
+		"mongodb.name":     "api-gateway",
+	}, "."), nil)
 
 }
 
 // MongoDBInit create mongodb connection and assign it back to var MongoDB above
 func MongoDBInit() {
+	var err error
 
+	mongodsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		Config.MustString("mongodb.user"),
+		Config.String("mongodb.password"),
+		Config.MustString("mongodb.host"),
+	)
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongodsn))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	MongoClient = client
+
+	MongoDB = client.Database(Config.MustString("mongodb.name"))
 }
--- test/mongodb_test.go
+++ test/mongodb_test.go
@@ -1,9 +1,30 @@
 package test
 
-func TestMongoDB() {
+import (
+	"context"
+	"testing"
+
+	"github.com/istt/api_gateway/internal/app"
+)
+
+func TestMongoDB(t *testing.T) {
+
 	app.MongoDBConfig()
 
 	app.MongoDBInit()
 
 	// try access mongodb to do something
+	dbnames, err := app.MongoClient.ListDatabaseNames(context.TODO(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(dbnames)
+
+	// test db
+	app.MongoDB.ListCollectionNames(context.TODO(), nil)
+	if err != nil {
+		t.Fatal(err)
+
+	}
+	t.Log(dbnames)
 }
