6c755219beb9062e6558b656539b690e6ddfd449
Dylan2011-pc <duyl0043@gmail.com>
2021-05-17 09:22:45 +0700

    Add docker files'

--- test/mongodb_test.go
+++ test/mongodb_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/istt/api_gateway/internal/app"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func TestMongoDB(t *testing.T) {
@@ -14,7 +15,7 @@ func TestMongoDB(t *testing.T) {
 	app.MongoDBInit()
 
 	// try access mongodb to do something
-	dbnames, err := app.MongoClient.ListDatabaseNames(context.TODO(), nil)
+	dbnames, err := app.MongoClient.ListDatabaseNames(context.TODO(), bson.D{})
 	if err != nil {
 		t.Fatal(err)
 	}
